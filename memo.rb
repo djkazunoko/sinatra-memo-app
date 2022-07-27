f7b614eb589b4956c2f457e8521a29e8c068fbaa
kazunoko1606 <djkazunoko@gmail.com>
2022-07-28 08:20:12 +0900

    特定のメモを表示する機能を実装

--- memo.rb
+++ memo.rb
@@ -4,7 +4,7 @@ require 'json'
 
 get '/' do
   File.open("public/memos.json") do |file|
-    @hash = JSON.load(file)
+    @memos = JSON.load(file)
   end
   erb :index
 end
@@ -16,4 +16,13 @@ end
 post '/memos' do
   @title = params[:title]
   erb :index
+end
+
+get '/memos/:id' do |n|
+  File.open("public/memos.json") do |file|
+    @memos = JSON.load(file)
+  end
+  @title = @memos[n]["title"]
+  @content = @memos[n]["content"]
+  erb :show
 end
