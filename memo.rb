ab77379e5e3159ba0477a4175029075054a45e2a
kazunoko1606 <djkazunoko@gmail.com>
2022-07-29 13:19:31 +0900

    rubocop-fjordをパスするように修正

--- memo.rb
+++ memo.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'cgi'
 
-FILE_PATH = "public/memos.json"
+FILE_PATH = 'public/memos.json'
 
 def get_memos(file_path)
-  File.open(file_path) { |f| JSON.load(f) }
+  File.open(file_path) { |f| JSON.parse(f.read) }
 end
 
 def set_memos(file_path, memos)
@@ -33,20 +35,18 @@ post '/memos' do
   memos = get_memos(FILE_PATH)
   maxid = 0
   memos.each_key do |id|
-    if id.to_i > maxid
-      maxid = id.to_i
-    end
+    maxid = id.to_i if id.to_i > maxid
   end
   id = (maxid + 1).to_s
-  memos[id] = {"title" => @title, "content" => @content}
+  memos[id] = { 'title' => @title, 'content' => @content }
   set_memos(FILE_PATH, memos)
   redirect '/memos'
 end
 
 get '/memos/:id' do |n|
   @memos = get_memos(FILE_PATH)
-  @title = @memos[n]["title"]
-  @content = @memos[n]["content"]
+  @title = @memos[n]['title']
+  @content = @memos[n]['content']
   erb :show
 end
 
@@ -60,8 +60,8 @@ end
 
 get '/memos/:id/edit' do |n|
   memos = get_memos(FILE_PATH)
-  @title = memos[n]["title"]
-  @content = memos[n]["content"]
+  @title = memos[n]['title']
+  @content = memos[n]['content']
   erb :edit
 end
 
@@ -70,7 +70,7 @@ patch '/memos/:id' do |n|
   @content = params[:content]
 
   memos = get_memos(FILE_PATH)
-  memos[n] = {"title" => @title, "content" => @content}
+  memos[n] = { 'title' => @title, 'content' => @content }
   set_memos(FILE_PATH, memos)
 
   redirect "/memos/#{n}"
