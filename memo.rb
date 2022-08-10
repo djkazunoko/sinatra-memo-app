e88378924f1ac20de87698a63054696e5bbf4279
kazunoko1606 <djkazunoko@gmail.com>
2022-08-10 15:01:59 +0900

    メモを1件表示

--- memo.rb
+++ memo.rb
@@ -24,6 +24,15 @@ get '/memos/new' do
   erb :new
 end
 
+get '/memos/:id' do
+  conn = get_connection
+  result = conn.exec("SELECT * FROM memos WHERE id = #{params[:id]}")
+  memo = result.tuple_values(0)
+  @title = memo[1]
+  @content = memo[2]
+  erb :show
+end
+
 post '/memos' do
   @title = params[:title]
   @content = params[:content]
@@ -33,13 +42,6 @@ post '/memos' do
   memos[id] = { 'title' => @title, 'content' => @content }
   set_memos(FILE_PATH, memos)
   redirect '/memos'
-end
-
-get '/memos/:id' do
-  @memos = get_memos(FILE_PATH)
-  @title = @memos[params[:id]]['title']
-  @content = @memos[params[:id]]['content']
-  erb :show
 end
 
 delete '/memos/:id' do
